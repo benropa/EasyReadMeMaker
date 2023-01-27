f4037466d86c15329093a737922adcf26ee22322
Ben Ropa <benropa@outlook.com>
2023-01-27 16:21:24 -0600

    adding in the md template

--- develop/index.js
+++ develop/index.js
@@ -9,13 +9,48 @@ const askUser = () => {
      return inquirer.prompt([
             {
                 type: 'input',
-                message: 'What is your github username?',
-                name: 'GitHub',
+                message: 'What is the name of your project?',
+                name: 'projectName',
             },
             {
                 type: 'input',
-                message: 'Add a description to your Read Me file:',
-                name: 'description',
+                message: 'What is the purpose of your application?',
+                name: 'purpose',
+            },
+            {
+                type: 'input',
+                message: 'Why did you build this project?',
+                name: 'build',
+            },
+            {
+                type: 'input',
+                message: 'What did you learn while creating this app?',
+                name: 'skills',
+            },
+            {
+                type: 'input',
+                message: 'What is necessary to use your project?',
+                name: 'useProject',
+            },
+            {
+                type: 'input',
+                message: 'Are there any other collaborators you would like to include? List them here:',
+                name: 'credits',
+            },
+            {
+                type: 'input',
+                message: 'Name the coding programs and languages you used here:',
+                name: 'programs',
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'contact',
             },
         ])
     };
@@ -25,9 +60,25 @@ askUser();
 // Next we need to generate a ReadMe.md file
 
 const createMDfile = (Ui) => {
-    return `# ${Ui.Github}
-    ## ${Ui.description}
-`
+    return `Project Title: # ${Ui.projectName}
+    ## Description
+    Please outline the purpose of this application, your motivation for creating the project, and what you learned while building it. 
+    - ${Ui.purpose}
+    - ${Ui.build}
+    - ${Ui.skills}
+    ## Table of Contents
+    For more complex projects, consider including a table of contents to simplify the navigation process for a user
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    ## Installation
+    - Usage
+    - License
+    - Contributing
+    - Tests
+    - Questions
+    `      
 }
 
 const init = () => {
