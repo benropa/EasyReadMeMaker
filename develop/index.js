7c74c9da54a1f1734b783a79ba5b7cd9826b995f
Ben Ropa <benropa@outlook.com>
2023-01-27 14:00:54 -0600

    removed html and css files

--- develop/index.js
+++ develop/index.js
@@ -2,54 +2,23 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-function writeHTML( {name, location, bio, linkedInUser, gitHubUser} ) {
-    const htmlString = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="./styles.css">
-            <title>My Portfolio</title>
-        </head>
-        <body>
-            <header >
-                <h1>
-                    ${name}
-                </h1>
-            </header>
-            <main>
-                <div>
-                    <h2>
-                        Bio:
-                    </h2>
-                    <p>
-                        ${bio}
-                    </p>
-                </div>
-            </main>
-            <footer >
-                <div>
-                    ${location}
-                </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/${linkedInUser}">LinkedIn</a>
-                </div>
-                <div>
-                    <a href="https://www.github.com/${gitHubUser}">GitHub</a>
-                </div>
-            </footer>
-        </body>
-        </html>`
-    return htmlString
+function writeMd({GitHub, description}) {
+    const md =`
+    # ${GitHub}
+
+    ## ${description}`
+   
+    return md; 
 }
 
-function writeHTMLFile(htmlString) {
-    fs.writeFile('index.html', htmlString, (err) => {
+           
+
+function writeMdFile(MdString) {
+    fs.writeFile('index.md', MdString, (err) => {
         if (err) {
             console.error(err)
         } else {
-            console.log('Success: HTML File Generated!')
+            console.log('Success: Md File Generated!')
         }
     })
 }
@@ -59,42 +28,25 @@ function main() {
         .prompt([
             {
                 type: 'input',
-                message: 'What is your name?',
-                name: 'name',
+                message: 'What is your github username?',
+                name: 'GitHub',
             },
             {
                 type: 'input',
-                message: 'What is your current location?',
-                name: 'location',
-            },
-            {
-                type: 'input',
-                message: 'Tell us about yourself.',
-                name: 'bio',
-            },
-            {
-                type: 'input',
-                message: 'What is your LinkedIn username?',
-                name: 'linkedInUser',
-            },
-            {
-                type: 'input',
-                message: 'What is your Github username?',
-                name: 'gitHubUser',
+                message: 'Add a description of your project:',
+                name: 'description',
             },
         ])
+
         .then((response) => {
             const prompt = {
-                name: response.name,
-                location: response.location,
-                bio: response.bio,
-                linkedInUser: response.linkedInUser,
-                gitHubUser: response.gitHubUser
+                GitHub: response.name,
+                description: response.location,
             }
 
-            const html = writeHTML(prompt);
+            const html = writeMd(prompt);
 
-            writeHTMLFile(html);
+            writeMdFile(md);
 
         });
 }
